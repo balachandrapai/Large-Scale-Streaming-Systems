7cb3058fd7653ae3190406662903acd3237ef93d
balachandrapai <balachandra.pai95@gmail.com>
2017-04-09 12:49:00 +0200

    First commit

---
+++ src/main/java/SparkStreamerJob.java
@@ -0,0 +1,80 @@
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaPairReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka.KafkaUtils;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Pai on 09-04-2017.
+ */
+public class SparkStreamerJob {
+
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    private SparkStreamerJob() {
+    }
+
+    private static void setLogLevels() {
+        boolean log4jInitialized = Logger.getRootLogger().getAllAppenders().hasMoreElements();
+        if (!log4jInitialized) {
+            // We first log something to initialize Spark's default logging, then we override the
+            // logging level.
+            Logger.getLogger(SparkStreamerJob.class).info("Setting log level to [WARN] for streaming job" +
+                    " To override add a custom log4j.properties to the classpath.");
+            Logger.getRootLogger().setLevel(Level.WARN);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        System.setProperty("hadoop.home.dir", "c:/winutils/");
+
+
+        if (args.length < 4) {
+            System.err.println("Usage: StreamingJob <zkQuorum> <group> <topics> <numThreads>");
+            System.exit(1);
+        }
+
+        setLogLevels();
+
+        SparkConf sparkConf = new SparkConf().setAppName("StreamingJob").setMaster("local[16]");
+        // Create the context with 2 seconds batch size
+        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(10));
+
+        int numThreads = Integer.parseInt(args[3]);
+        Map<String, Integer> topicMap = new HashMap<>();
+        String[] topics = args[2].split(",");
+        for (String topic : topics) {
+            topicMap.put(topic, numThreads);
+        }
+
+
+        JavaPairReceiverInputDStream<String, String> messages =
+                KafkaUtils.createStream(jssc, args[0], args[1], topicMap);
+
+
+
+        JavaDStream<String> lines = messages.map(Tuple2::_2);
+
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(SPACE.split(x)).iterator());
+
+        JavaPairDStream<String, Integer> wordCounts = words.mapToPair(s -> new Tuple2<>(s, 1))
+                .reduceByKey((i1, i2) -> i1 + i2);
+
+        wordCounts.print();
+        jssc.start();
+        jssc.awaitTermination();
+    }
+
+
+}
