f406f7b0aec7605baea08551dc1031d4fb9f406d
balachandrapai <balachandra.pai95@gmail.com>
2017-04-09 13:50:11 +0200

    Renamed

--- src/main/java/KafkaSender.java
+++ src/main/java/KafkaSender.java
@@ -6,7 +6,7 @@ import java.util.Properties;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-class SimpleProducer {
+class KafkaSender {
 
     public static void main(String[] args) throws Exception{
 
